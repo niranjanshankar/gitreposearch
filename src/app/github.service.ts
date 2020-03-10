efde35c5a6694d331c334b8fc8fc9ecf58ef3ad9
Niranjan Shankar <niranjan.shankar@imr.ie>
2020-03-10 14:42:37 +0000

    enable pagination with maximum 4 buttons: prev, next, first & last

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -12,8 +12,15 @@ export class GithubService {
 
   constructor(private http: HttpClient) { }
 
+  // observing response to tap into the response headers for pagination
+  // Limiting to 15 response per page. This call returns first page by default
   public getRepositoriesByUsername(userName: string): Observable<any> {
-    return this.http.get(this.apiUrl + '/users/' + userName + '/repos');
+    return this.http.get(this.apiUrl + '/users/' + userName + '/repos?per_page=15', { observe: 'response' });
+  }
+
+  // observing response to tap into the response headers for pagination
+  public getRepositories(url: string): Observable<any> {
+    return this.http.get(url, { observe: 'response' });
   }
 
   public getContributorsByRepoName(owner: string, repo: string): Observable<any> {
--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -12,6 +12,11 @@ export class RepositoriesComponent implements OnInit {
   // Could use an interface to model the response
   public repositories: Array<Object>;
   public isError: boolean;
+  // Could be moved to a separate interface
+  public pages: Array<{
+    url: string;
+    title: string;
+  }>;
 
   constructor(private githubService: GithubService) { }
 
@@ -20,11 +25,45 @@ export class RepositoriesComponent implements OnInit {
 
   public searchUserRepositories(): void {
     this.isError = false;
-    this.githubService.getRepositoriesByUsername(this.searchTerm.trim()).subscribe(res => {
-      this.repositories = res;
+    this.githubService.getRepositoriesByUsername(this.searchTerm.trim()).subscribe(response => {
+      this.buildReposAndPagination(response);
     }, err => {
       this.isError = true;
     });
+  }
+
+  public getRepositories(url: string): void {
+    this.githubService.getRepositories(url).subscribe(response => {
+      this.buildReposAndPagination(response);
+    }, err => {
+      this.isError = true;
+    });
+  }
+
+  private buildReposAndPagination(response): void {
+    this.repositories = response.body;
+
+    // generate pagination based on the response header
+    const str = response.headers.get('link');
+    if (str && str !== null) {
+      this.buildPaginationButtons(str);
+    }
+  }
+
+  // This method parses the response header "links" and
+  // build the object that is used for pagination buttons
+  // Reference: https://www.youtube.com/watch?v=5QlE6o-iYcE
+  private buildPaginationButtons(str: string): void {
+    const links = str.split(',');
+    const urls = links.map(link => {
+      // Hack to remove unwanted string elements
+      // If there was time, this could be replaced by REGEX
+      return {
+        url: link.split(';')[0].replace('<', '').replace('>', '').trim(),
+        title: link.split(';')[1].replace('rel="', '').replace('"', '').trim()
+      };
+    });
+    this.pages = urls;
   }
 
 }
